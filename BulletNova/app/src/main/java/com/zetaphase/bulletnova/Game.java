04a9b96937bec6296f681367e88d15d9163509f4
daveho8888 <daveho8888@gmail.com>
2017-02-06 20:25:40 -0800

    initial commit

---
+++ BulletNova/app/src/main/java/com/zetaphase/bulletnova/Game.java
@@ -0,0 +1,18 @@
+package com.zetaphase.bulletnova;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Game extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // no title on top
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_game);
+    }
+}
