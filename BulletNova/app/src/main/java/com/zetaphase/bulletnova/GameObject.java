17db8f4d90af8392b2478e8376ca169d8454c163
daveho8888 <daveho8888@gmail.com>
2017-02-07 22:12:44 -0800

    add GameObject class

--- BulletNova/app/src/main/java/com/zetaphase/bulletnova/GameObject.java
+++ BulletNova/app/src/main/java/com/zetaphase/bulletnova/GameObject.java
@@ -1,9 +1,38 @@
 package com.zetaphase.bulletnova;
 
+import android.graphics.Rect;
+
 /**
  * Created by Dave Ho on 2/7/2017.
  */
 
 public abstract class GameObject {
-    
+    protected int x;
+    protected int y;
+    protected int dy;
+    protected int dx;
+    protected int width;
+    protected int height;
+
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public int getWidth(){
+        return width;
+    }
+    public Rect getRectangle(){
+        return new Rect(x, y, x+width, y+height);
+    }
 }
