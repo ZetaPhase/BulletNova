4b7f1a406764fbc5d31d1cb65a5f60828e78ef98
daveho8888 <daveho8888@gmail.com>
2017-02-10 10:30:34 -0800

    add counter to surfaceDestroyed

--- BulletNova/app/src/main/java/com/zetaphase/bulletnova/GamePanel.java
+++ BulletNova/app/src/main/java/com/zetaphase/bulletnova/GamePanel.java
@@ -37,13 +37,15 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceDestroyed(SurfaceHolder holder){
         boolean retry = true;
-        while(retry)
+        int counter = 0;
+        while(retry && counter<1000)
         {
+            counter ++;
             try{thread.setRunning(false);
                 thread.join();
+                retry = false;
 
             }catch(InterruptedException e){e.printStackTrace();}
-            retry = false;
         }
         //thread.interrupt();
 
